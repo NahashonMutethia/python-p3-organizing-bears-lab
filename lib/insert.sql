e795b11f89c7715d52018c282a3e20d721f2e981
nahashonmutethia <nashtesh22@gmail.com>
2024-06-12 00:06:15 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 3, 'F', 'Brown', 'Calm', 1),
+('Sergeant Brown', 8, 'M', 'Brown', 'Aggressive', 1),
+('Melissa', 4, 'F', 'Black', 'Calm', 1),
+('Grinch', 6, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 7, 'F', 'White', 'Playful', 1),
+(NULL, 2, 'F', 'Brown', 'Timid', 1);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,61 @@
+# Select all female bears and return their name and age
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex='F';
 """
 
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+# Select all bears' names and orders them alphabetically
+select_all_bears_names_alphabetically = """
+    SELECT
+        bears.name
+    FROM bears
+    ORDER BY bears.name ASC;
 """
 
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+# Select all bears' names and ages that are alive and order them by age
+select_all_alive_bears_return_name_and_age_order_by_age = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE alive=1
+    ORDER BY bears.age ASC;
 """
 
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+# Select the oldest bear and return its name and age
+select_oldest_bear_return_name_and_age = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age DESC
+    LIMIT 1;
 """
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+
+# Select the youngest bear and return its name and age
+select_youngest_bear_return_name_and_age = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age ASC
+    LIMIT 1;
+"""
+
+# Select the bear with the name "Sergeant Brown" and return all columns
+select_sergeant_brown_return_all_columns = """
+    SELECT *
+    FROM bears
+    WHERE name='Sergeant Brown';
+"""
+
+# Select all bears that are alive and order them by temperament
+select_all_alive_bears_order_by_temperament = """
+    SELECT *
+    FROM bears
+    WHERE alive=1
+    ORDER BY temperament;
 """
